bc73b2532df66a77276e69806a5847c09cd8f1da
Jorge <j-anaya-@hotmail.com>
2023-01-05 23:11:15 -0500

    adding server file storage

--- server/index.js
+++ server/index.js
@@ -24,3 +24,16 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/assets", express.static(path.join(dirname, "public/assets")));
+
+// FILE STORAGE
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/assets");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage });
